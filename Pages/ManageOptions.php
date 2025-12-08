22fd1bea6ade8cde3e9c600c8abe5babc7e71f8b
COLANT Alan <alan.colant@uni-deal.com>
2025-12-08 03:43:03 +0100

    feat: allow options to be resetted in admin

--- Pages/ManageOptions.php
+++ Pages/ManageOptions.php
@@ -9,6 +9,7 @@ use Epsicube\Schemas\Exporters\FilamentExporter;
 use Epsicube\Support\Facades\Modules;
 use Epsicube\Support\Facades\Options;
 use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 use Filament\Forms\Components\Field;
 use Filament\Infolists\Components\Entry;
 use Filament\Notifications\Notification;
@@ -22,6 +23,7 @@ use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Schemas\Schema;
+use Filament\Support\Enums\IconPosition;
 use Filament\Support\Enums\Operation;
 use Filament\Support\Enums\Size;
 use Filament\Support\Icons\Heroicon;
@@ -75,10 +77,18 @@ class ManageOptions extends Page implements HasSchemas
     {
 
         return $schema->statePath('data')->operation($this->operation->value)->schema(function () {
-            $exporter = new FilamentExporter($this->operation, function (Component $component, ?string $name): void {
+            $optionsSchema = Options::getSchema($this->activeTab);
+
+            $exporter = new FilamentExporter($this->operation, function (Component $component, ?string $name) use ($optionsSchema): void {
+
                 if ($name === null || ! ($component instanceof Field || $component instanceof Entry)) {
                     return;
                 }
+                $default = $optionsSchema->properties()[$name]?->getDefault() ?? null;
+                if ($default !== null && $this->operation === Operation::Edit && method_exists($component, 'placeholder')) {
+                    $component->placeholder($default);
+                }
+
                 $component->afterContent(function (Component $component) use ($name) {
                     $isOverride = isset($this->stored[$name]) && $component->getState() !== null;
 
@@ -89,11 +99,11 @@ class ManageOptions extends Page implements HasSchemas
                                 ->icon(Heroicon::OutlinedArrowUturnLeft)
                                 ->color('warning')
                                 ->action(fn () => $component->state(null))
-                            : Icon::make(Heroicon::OutlinedExclamationTriangle)
+                            : Icon::make(Heroicon::OutlinedLockClosed)
                                 ->color('warning')
                                 ->tooltip(__('Overridden value')),
                     ] : [
-                        Icon::make(Heroicon::OutlinedCube)->color('gray')->tooltip(
+                        Icon::make(Heroicon::OutlinedLockOpen)->color('gray')->tooltip(
                             $this->operation === Operation::Edit
                            ? __('Leave empty to keep the module default')
                            : __('Default value provided by the module')
@@ -102,7 +112,7 @@ class ManageOptions extends Page implements HasSchemas
                 });
             });
 
-            return Options::getSchema($this->activeTab)->export($exporter);
+            return $optionsSchema->export($exporter);
         });
     }
 
@@ -119,17 +129,28 @@ class ManageOptions extends Page implements HasSchemas
     protected function getHeaderActions(): array
     {
         return [
-            Action::make('toggleMode')
-                ->label(fn () => $this->operation === Operation::View ? __('Edit Mode') : __('View Mode'))
-                ->icon(fn () => $this->operation === Operation::View ? Heroicon::OutlinedPencil : Heroicon::OutlinedEye)
-                ->outlined()->size(Size::Small)
-                ->color(fn () => $this->operation === Operation::View ? 'primary' : 'gray')
-                ->action(function () {
-                    $this->operation = ($this->operation === Operation::View)
-                        ? Operation::Edit
-                        : Operation::View;
-                    $this->fillForm();
-                }),
+            ActionGroup::make([
+                Action::make('displayMode')
+                    ->disabled()
+                    ->label(fn () => $this->operation === Operation::Edit ? __('Edit Mode') : __('View Mode'))
+                    ->icon(fn () => $this->operation === Operation::Edit ? Heroicon::OutlinedPencil : Heroicon::OutlinedEye)
+                    ->outlined()->size(Size::Small)
+                    ->color(fn () => $this->operation === Operation::Edit ? 'warning' : 'gray'),
+
+                Action::make('toggleMode')
+                    ->label(__('Switch'))
+                    ->color('primary')
+                    ->icon(Heroicon::OutlinedArrowsRightLeft)->iconPosition(IconPosition::After)
+                    ->outlined()
+                    ->size(Size::Small)
+                    ->action(function () {
+                        $this->operation = $this->operation === Operation::Edit
+                            ? Operation::View
+                            : Operation::Edit;
+
+                        $this->fillForm();
+                    }),
+            ])->buttonGroup(),
         ];
     }
 
